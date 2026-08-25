27228373ecd6236c043aebbc42a3ccb16bfa5dbf
Natanael025 <natanvieira2501@gmail.com>
2026-08-25 08:37:52 -0300

    Create App component with login UI
    Initial implementation of the App component with login functionality, including email and password input fields.

---
+++ app.jsx
@@ -0,0 +1,67 @@
+import { StyleSheet, Text, View, Pressable, Image, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        style={styles.logo}
+        source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7C0NlbHXsMpd67_3fP8sauaf7Xy2cDkCNMVcN6imz158sgpE0eFyx_5xK&s=10'}}
+      />
+
+      <View style={{marginBottom: 10}}>
+        <Text>Seu email: </Text>
+        <TextInput
+        placeholder='Digite aqui'
+        style={styles.input}
+        />
+      </View>
+
+      <View style={{marginBottom: 10}}>
+        <Text>Sua senha: </Text>
+        <TextInput
+        secureTextEntry={true}
+        placeholder='Digite aqui'
+        style={styles.input}
+        />
+      </View>
+      
+
+      <Pressable style={styles.botao_login}>Login</Pressable>
+      <Pressable style={{fontSize: 12}}>Não tem conta? Crie uma!</Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+   logo: {
+    width: 90,
+    height: 90,
+    borderRadius: 10,
+    marginBottom: 50,
+    borderWidth: 2
+  },
+  botao_login: {
+    width: 80,
+    borderRadius: 5,
+    display: 'flex',
+    alignItems: 'center',
+    padding: 10,
+    margin: 5,
+    backgroundColor: 'black',
+    color: 'white'
+  },
+  input:{
+    borderWidth: 1,
+    borderRadius: 5,
+    color: '#a0a0a0',
+    padding: 3,
+    width: 300
+  },
+});
